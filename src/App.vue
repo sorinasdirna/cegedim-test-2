f887c333a0768c43b9bbf167901223a4280fee21
Sorina <ssdirna@gmail.com>
2021-06-09 20:00:43 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from "./components/Form";
+
+export default {
+  name: "App",
+  components: {
+    Form
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background: #e2e2e2;
+}
+</style>
